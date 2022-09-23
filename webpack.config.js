04279ccb65b1f3994ed7a1950cc92c38535e9e6a
negi524 <egtnrumpbiizarre.info@gmail.com>
2022-09-23 18:02:43 +0900

    npm script追加

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,7 @@ module.exports = {
       {
         test: /\.s[ac]ss$/i,
         use: [
-          // Creates `style` nodes from JS strings
-          // "style-loader",
+          // コンパイルの際にjsとcssファイルを分ける
           MiniCssExtractPlugin.loader,
           // Translates CSS into CommonJS
           "css-loader",
