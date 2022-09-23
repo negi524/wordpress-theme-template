6060391b768686195444f7714ee05f701a530728
negi524 <egtnrumpbiizarre.info@gmail.com>
2022-09-23 18:04:50 +0900

    source mapを表示するように設定

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+  devtool: "source-map",
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
