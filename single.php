e3e569826688ff204d56850c1abd49cfe72717b2
negi524 <egtnrumpbiizarre.info@gmail.com>
2022-09-24 13:11:04 +0900

    テーマの初期設定 (#2)
    * テーマとして最低限必要なファイル作成
    
    * 基本的なテンプレートページ作成

---
+++ single.php
@@ -0,0 +1,6 @@
+<!-- 投稿ページ -->
+<?php get_header(); ?>
+  <main>
+    <p>投稿ページです</p>
+  </main>
+<?php get_footer(); ?>
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 // スタイルシート（Sass）を読み込む
+// .scssファイルを新規追加した場合はここに記載しないとコンパイルされない
 import './style.scss';
+import './header.scss'
+import './footer.scss'
