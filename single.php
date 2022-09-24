55008bc40bd07d00c75aa1afaac085b562f5a516
negi524 <egtnrumpbiizarre.info@gmail.com>
2022-09-24 15:09:37 +0900

    投稿ページのコンテンツ作成 (#5)
    * 投稿ページの内容作成
    
    * 投稿日を表示する

--- single.php
+++ single.php
@@ -1,6 +1,19 @@
 <!-- 投稿ページ -->
 <?php get_header(); ?>
-  <main>
-    <p>投稿ページです</p>
-  </main>
+  <div class="container">
+    <div class="row">
+      <main class="col-sm-9">
+        <?php while(have_posts()): the_post(); ?>
+          <h2><?php the_title(); ?></h2>
+          <p><?php the_date(); ?></p>
+          <div>
+            <?php the_content(); ?>
+          </div>
+        <?php endwhile; ?>
+      </main>
+      <div class="col-sm-3">
+        <?php get_sidebar(); ?>
+      </div>
+    </div>
+  </div>
 <?php get_footer(); ?>
