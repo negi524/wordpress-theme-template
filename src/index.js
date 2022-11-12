2dd95349ac7d221fca0381b859772b29fb951642
negi524 <egtnrumpbiizarre.info@gmail.com>
2022-11-12 15:44:23 +0900

    prettierrc適用

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "bootstrap";
 
 // スタイルシート（Sass）を読み込む
 // .scssファイルを新規追加した場合はここに記載しないとコンパイルされない
-import './style.scss';
-import './header.scss'
-import './footer.scss'
-import './sidebar.scss'
+import "./style.scss";
+import "./header.scss";
+import "./footer.scss";
+import "./sidebar.scss";
