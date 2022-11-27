927156161a20034e4e5f0b60025a1951188d0c7c
negi524 <egtnrumpbiizarre.info@gmail.com>
2022-11-27 16:04:30 +0900

    言語追加

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ import bash from "highlight.js/lib/languages/bash";
 import typescript from "highlight.js/lib/languages/typescript";
 import sql from "highlight.js/lib/languages/sql";
 import java from "highlight.js/lib/languages/java";
-import "highlight.js/styles/github.css";
+import css from "highlight.js/lib/languages/css";
+import scss from "highlight.js/lib/languages/scss";
+import json from "highlight.js/lib/languages/json";
+import yaml from "highlight.js/lib/languages/yaml";
+import "highlight.js/styles/atom-one-dark.css";
 
 hljs.registerLanguage("javascript", javascript);
 hljs.registerLanguage("python", python);
@@ -24,6 +28,10 @@ hljs.registerLanguage("bash", bash);
 hljs.registerLanguage("typescript", typescript);
 hljs.registerLanguage("sql", sql);
 hljs.registerLanguage("java", java);
+hljs.registerLanguage("css", css);
+hljs.registerLanguage("scss", scss);
+hljs.registerLanguage("json", json);
+hljs.registerLanguage("yaml", yaml);
 
 document.addEventListener("DOMContentLoaded", (event) => {
   document.querySelectorAll("pre code").forEach((el) => {
