56762d8b95dfcc206cddf99cd8f77e5ab4e4d002
negi524 <egtnrumpbiizarre.info@gmail.com>
2023-06-15 12:58:59 +0900

    ヘッダー、フッターをリファクタ (#13)
    * ヘッダーとフッターの共通部分をリファクタ
    
    * タイトルの出力
    
    * ヘッダーコンポーネント作成

--- page.php
+++ page.php
@@ -1,6 +1,14 @@
 <!-- 固定ページ -->
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <?php get_header(); ?>
+</head>
+<body>
+  <?php get_template_part( 'components/header') ?>
   <main>
     <p>固定ページです</p>
   </main>
-<?php get_footer(); ?>
+  <?php get_footer(); ?>
+</body>
+</html>
